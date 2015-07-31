2d5eb3cc72e19f4d6703ea4a82c0207f31c9574c
AlinaJahnes & Christine Schatz <pair+AlinaJahnes.ChristineSchatz@devbootcamp.com>
2015-07-31 19:23:20 -0400

    add current user helper method

--- stack_overflow_clone/app/helpers/application_helper.rb
+++ stack_overflow_clone/app/helpers/application_helper.rb
@@ -2,4 +2,9 @@ module ApplicationHelper
     def user_can_edit(user_id)
       return (!session[:user_id].nil? && session[:user_id] == user_id.to_i)
     end
+
+    def current_user
+      User.find_by(id: session[:user_id])
+    end
+
 end
