dae6530734e053b2a7e108bb14e2ac72fb43dd37
Sihong31 <sihongspring2011@gmail.com>
2015-07-31 10:56:21 -0400

    added dependent destroy

--- stack_overflow_clone/app/models/question.rb
+++ stack_overflow_clone/app/models/question.rb
@@ -1,8 +1,8 @@
 class Question < ActiveRecord::Base
 	belongs_to :user
 	has_many :votes, as: :voteable
-	has_many :answers
-	has_many :comments, as: :commentable
+	has_many :answers, dependent: :destroy
+	has_many :comments, as: :commentable, dependent: :destroy
 	has_many :question_tags
 	has_many :tags, through: :question_tags
 
