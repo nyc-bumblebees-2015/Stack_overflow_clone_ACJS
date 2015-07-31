f556b410b4eba3017c4c21cc69637d080a49b2db
Christine Schatz <cmschatz@gmail.com>
2015-07-31 12:00:41 -0400

    Create resources in routes.rb

--- stack_overflow_clone/config/routes.rb
+++ stack_overflow_clone/config/routes.rb
@@ -1,56 +1,24 @@
 Rails.application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
 
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+root 'questions#index'
 
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
+ resources :users, except: [:new, :destroy]
 
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
+ resources :questions
 
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
+ resources :answers, except: [:index, :show, :destroy]
 
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
+ resources :comments, except: [:index, :show, :destroy]
 
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
+ resources :tags, only: [:index]
 
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
+ resources :votes, only: [:create, :new]
 
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
 
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
+ get '/login' => 'sessions#new'
+ post '/login' => 'sessions#create'
+ get '/signup' => 'users#new'
+ get '/logout' => 'sessions#destroy'
+
+
 end
