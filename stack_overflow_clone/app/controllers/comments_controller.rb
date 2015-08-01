997d88ca795f51154b3f0a0a33eac0fcbc955dfd
Sihong31 <sihongspring2011@gmail.com>
2015-08-01 11:39:12 -0400

    debugged comments controller on question show page

--- stack_overflow_clone/app/controllers/comments_controller.rb
+++ stack_overflow_clone/app/controllers/comments_controller.rb
@@ -5,18 +5,20 @@ class CommentsController < ApplicationController
   end
 
   def create
-    if params[:commentable_type] == 'Question'
-      @commentable = Question.find_by(id: params[:question_id])
+    if params[:comment][:commentable_type] == 'Question'
+      @commentable = Question.find_by(id: params[:comment][:commentable_id])
+      @comment = @commentable.comments.create(comment_params)
+      redirect_to question_path(@commentable)
     else
-      @commentable = Answer.find_by(id: params[:answer_id])
+      @commentable = Answer.find_by(id: params[:comment][:commentable_id])
+      @comment = @commentable.comments.create(comment_params)
+      redirect_to question_path(@commentable.question)
     end
-
-    @comment = @commentable.comments.build(comment_params)
 
   end
 
   private
   def comment_params
-    params.require(:comment).permit(:body).merge(user_id: session[:user_id])
+    params.require(:comment).permit(:body, :commentable_type, :commentable_id).merge(user_id: session[:user_id])
   end
 end
