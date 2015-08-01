ef955997d23192d107eaa4773b8a6beca0fcb34a
Christine Schatz <cmschatz@gmail.com>
2015-08-01 15:34:00 -0400

    implement find_user method to generate user's profile based on search

--- stack_overflow_clone/app/controllers/users_controller.rb
+++ stack_overflow_clone/app/controllers/users_controller.rb
@@ -23,6 +23,11 @@ class UsersController < ApplicationController
     @user = User.find_by(id: params[:id])
   end
 
+  def find_user
+    @user = User.find_by(username: params[:username])
+    render :show
+  end
+
   private
   def user_params
     params.require(:user).permit(:first_name, :last_name, :email, :username, :password)
