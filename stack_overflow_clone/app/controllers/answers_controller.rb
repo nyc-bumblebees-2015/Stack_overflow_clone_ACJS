0f9479e783aad7640ae60156e257bce1b46e2271
Sihong31 <sihongspring2011@gmail.com>
2015-08-01 12:50:40 -0400

    answers update via ajax

--- stack_overflow_clone/app/controllers/answers_controller.rb
+++ stack_overflow_clone/app/controllers/answers_controller.rb
@@ -7,7 +7,7 @@ class AnswersController < ApplicationController
     answer = Answer.new(answer_params)
     if answer.save
       question = Question.find_by(id: answer.question_id)
-      redirect_to question_path(question)
+      request.xhr? ? render(partial: 'answer', layout: false, locals: {answer: answer}) : redirect_to(question)
     else
       flash.now[:error] = answer.errors.full_messages
       render :new
