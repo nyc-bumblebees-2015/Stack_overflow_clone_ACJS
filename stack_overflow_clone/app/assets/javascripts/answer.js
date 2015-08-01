54a555a3ca9dde0d3abd483dd90c357ec853abab
Sihong31 <sihongspring2011@gmail.com>
2015-08-01 12:48:12 -0400

    created answer partial

---
+++ stack_overflow_clone/app/assets/javascripts/answer.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+  $("new_answer").on('submit', function(event){
+    event.preventDefault();
+    $.ajax({
+      url: event.target.action,
+      method: event.target.method,
+      data: $(event.target).serialize()
+    }).done(function(answerPartial){
+      $(".answer-container").append(answerPartial)
+      $("new_answer").trigger("reset")
+    }).fail(function(error){
+      console.log(error, "hidden panda")
+    })
+  })
+})
