87ba00536af1c790e7e473de274784790c0d90bd
ChristineSchatz <cmschatz@gmail.com>
2015-08-01 18:55:20 -0400

    add spec files

--- stack_overflow_clone/spec/controllers/question_controller_spec.rb
+++ stack_overflow_clone/spec/controllers/question_controller_spec.rb
@@ -1,2 +1,36 @@
-require 'rails_helper'
+require 'spec_helper'
 
+describe QuestionsController do
+  let (:sample_question) { FactoryGirl.create(:question) }
+  before(:each) do
+    stub_authorize_user!
+  end
+
+  it "#index" do
+    get :index
+    expect(assigns(:questions)).to eq Question.all
+  end
+
+  it "#new" do
+    get :new
+    expect(assigns(:question)).to be_a_new(Question)
+  end
+
+  context "#create" do
+    it "creates a question with valid params" do
+      question :create, {question: {title: "yo", body: "hello", user_id: 1}}
+      expect(response).to redirect_to :root
+    end
+
+  end
+
+  context "#destroy" do
+    it "deletes the question" do
+      old_count = Question.count
+      delete :destroy, id: sample_question.id
+      expect(response).to redirect_to :root
+      expect(Question.count).to eq(old_count -1)
+      expect(Question.where(id: sample_question.id).count).to eq(0)
+    end
+  end
+end
--- stack_overflow_clone/spec/factories.rb
+++ stack_overflow_clone/spec/factories.rb
@@ -10,5 +10,6 @@ FactoryGirl.define do
   factory :question do
     title Faker::Lorem.sentence
     body Faker::Lorem.paragraph
+    user_id 1
   end
 end
