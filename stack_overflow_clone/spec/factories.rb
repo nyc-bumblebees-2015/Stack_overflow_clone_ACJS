b0cc00879c8a8a2a2be16766b2bc225cc13714a1
ChristineSchatz <cmschatz@gmail.com>
2015-08-01 17:54:12 -0400

    initial setup of rspec environment

---
+++ stack_overflow_clone/spec/factories.rb
@@ -0,0 +1,14 @@
+FactoryGirl.define do
+  factory :user do
+    sequence(:email) {|n| "email#{n}@gmail.com" }
+    username "cartman"
+    first_name "jeez"
+    last_name "shit"
+    password "123456"
+  end
+
+  factory :question do
+    title Faker::Lorem.sentence
+    body Faker::Lorem.paragraph
+  end
+end
