7ac3b5643663216df34e68cf418f6773dd99b628
Sihong31 <sihongspring2011@gmail.com>
2015-08-01 16:35:19 -0400

    implemented elapsed time method across views for question, show, comment

--- stack_overflow_clone/app/helpers/application_helper.rb
+++ stack_overflow_clone/app/helpers/application_helper.rb
@@ -7,4 +7,9 @@ module ApplicationHelper
       User.find_by(id: session[:user_id])
     end
 
+    def elapsed_time(datetime)
+      current_time = Time.now
+      distance_of_time_in_words(current_time,datetime)
+    end
+
 end
