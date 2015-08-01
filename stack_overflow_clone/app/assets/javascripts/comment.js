ba7e14bed4992385482417dbc7aa3c6f1b1c4f02
Sihong31 <sihongspring2011@gmail.com>
2015-08-01 12:27:31 -0400

    fully functional ajax update for adding comments

--- stack_overflow_clone/app/assets/javascripts/comment.js
+++ stack_overflow_clone/app/assets/javascripts/comment.js
@@ -1,16 +1,45 @@
 $(document).ready(function(){
+  $(".question-comment-form").hide()
+  $(".answer-comment-form").hide()
+
+  $(".question-comment-link").on('click', function(event){
+    event.preventDefault();
+    $(".question-comment-form").show()
+  })
+
   $(".question-comment-form").on('submit', function(event){
     event.preventDefault();
-  $.ajax({
-    url: event.target.action,
-    method: event.target.method,
-    data: $(event.target).serialize()
+    $.ajax({
+      url: event.target.action,
+      method: event.target.method,
+      data: $(event.target).serialize()
+    }).done(function(response){
+        $(".question-comment-container").append(response)
+        $(".new_comment").trigger('reset')
+        $(".question-comment-form").hide()
+    }).fail(function(error){
+        console.log(error, "fail panda")
+    })
   })
-  .done(function(response){
-    $(".question-comment-container").append(response)
+
+  $(".answer-comment-link").on('click', function(event){
+    event.preventDefault();
+    $(".answer-comment-form").show()
   })
-  .fail(function(error){
-    console.log(error, "fail panda")
+
+  $(".answer-comment-form").on('submit', function(event){
+    event.preventDefault();
+    $.ajax({
+      url: event.target.action,
+      method: event.target.method,
+      data: $(event.target).serialize()
+    }).done(function(response){
+      $(".answer-comment-container").append(response)
+      $(".new_comment").trigger('reset')
+      $(".answer-comment-form").hide()
+    }).fail(function(error){
+      console.log(error, "bad panda")
+    })
   })
- })
+
 })
